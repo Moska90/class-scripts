3375f55ce06f847bfe9418a734d7449e846f027e
Moska90 <57809899+Moska90@users.noreply.github.com>
2023-10-22 02:12:52 +0200

    Add files via upload

--- Blaus/blaus.sh
+++ Blaus/blaus.sh
@@ -10,12 +10,31 @@ BOLD="\e[1m"
 RESET="\e[0m"
 
 declare -A code
-code=(`cat "2lettercode.txt"`)
+function country_code() {
+        while IFS= read -r line; do
+                # Split each line into key and value using '=' as the delimiter
+                IFS='=' read -ra parts <<< "$line"
+                key="${parts[0]}"
+                country2="${parts[1]}"
+        
+                # Add the key-value pair to the associative array
+                code["${country2,,}"]="$key"
+        done < "2lettercode.txt"
 
-for key in ${!code[@]}; do
-    echo -e "$key"
-done
-read
+        read -p "Enter your country: " country
+
+        country="${country,,}"
+
+        # Search for the 2-letter code based on the entered country
+        code_found="${code[$country]}"
+
+        # Check if the code was found
+        if [ -n "$code_found" ]; then
+                return 0
+        else
+                echo "País no encontrado en la base de datos."
+        fi
+}
 
 function passwd-check() {
         if [[ "$passwd" == "$passwd2" ]]; then
@@ -23,17 +42,24 @@ function passwd-check() {
         else
                 return 1
         fi
-} 
+}
+
+function dir_creation() {
+        mkdir -p /var/www/$domain /var/www/$domain/html /var/www/$domain/log
+
+        touch /var/www/$domain/log/$domain.log
+        touch /var/www/$domain/log/$domain.error.log
+}
 
 clear
 
-echo -e " Bienvenido al script de automatización de DHM"
+echo -e "$LMAGENTA Bienvenido al script de automatización de DHM$RESET"
 sleep 1
-
 
 echo -e " Escribe tu nombre de usuario"
 read -p " >" user
 
+#Passwd
 while true; do
         echo -e " Escribe una contraseña para acceder"
         read passwd
@@ -52,23 +78,19 @@ read -p " >" domain
 
 clear
 
-echo -e "$LRED Perfecto, ya estas registrado $RESET"
+echo -e "$LRED Ya estas registrado $RESET"
 
-mkdir -p /var/www/$domain  /var/www/$domain/log
+LOGFILE="/var/www/$domain/log/$domain.log"
+ERRFILE="/var/www/$domain/log/$domain.error.log"
 
-chmod 750 /var/www/$domain
-chmod 750 /var/www/$domain/log
-
-LOGFILE="/var/www/$user/log/$domain.log"
-ERRFILE="/var/www/$user/log/$domain.error.log"
+dir_creation
 
 
-#Preguntas para instalacion personalizada
+#Preguntas para creación de la clave OpenSSL
 echo -e "$LGREEN Rellena las siguientes preguntas para terminar de configurar tu dominio,\n cuando hayas leido esto pulsa ENTER $RESET"
 read
 
-echo -e " "
-read -p " >" country >>$LOGFILE 2>$ERRFILE
+country_code
 
 echo -e " Provincia?"
 read -p " >" state >>$LOGFILE 2>$ERRFILE
@@ -92,52 +114,93 @@ echo -e " $company"
 echo -e " $domain"
 echo -e " $email"
 
-read -e -p "$LRED Estas seguro? [y/N] $RESET" response
+echo -e "$LRED Estas seguro? [y/N] $RESET"
+read -p "  " response
 case $response in
     [Yy]|[Yy][Ee][Ss])
-    ;;
+        ;;
     [Nn]|[Nn][Oo])
         echo -e "Exiting"
         exit
-    ;;
+        ;;
+    *)
+        echo -e "Exiting"
+        exit
+        ;;
 esac
 
 
 # Comando OpenSSL para generar un certificado
+echo -e "$LGREEN Generando tu certificado$RESET"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$domain.key -out /etc/ssl/certs/$domain.crt <<EOF >>$LOGFILE 2>$ERRFILE
-$country
+$code_found
 $state
 $city
+$company
 $company
 $domain
 $email
 EOF
 
+
 #Creando archivo de configuracion de servidor web
-echo -e "server {
+echo -e "$LGREEN Creando tu archivo de configuracion del servidor web$RESET"
+echo -e "
+server {
         listen 443 ssl;
         listen [::]:443 ssl;
+
         ssl_certificate /etc/ssl/certs/$domain.crt;
         ssl_certificate_key /etc/ssl/private/$domain.key;
         include snippets/ssl-params.conf;
 
         root /var/www/$domain/html;
-
         index index.html index.htm index.nginx-debian.html;
-
-        server_name $domain www.$domain;
+        server_name $domain     www.$domain;
 
         location / {
                 try_files \$uri \$uri/ =404;
         }
 }
-
 server {
         listen 80;
         listen [::]:80;
-
         server_name $domain www.$domain;
-
-        return 301 https://\$server_name\$request_uri;
+        return 302 https://\$server_name\$request_uri;
 }
 " >> /etc/nginx/sites-available/$domain
+
+cp /etc/nginx/sites-available/"$domain" /etc/nginx/sites-enabled/"$domain"
+
+cp index.html /var/www/$domain/html/
+
+#SFTP + User Configuration
+echo -e "$LGREEN Iniciando configuracion de SFTP y Usuario$RESET"
+chmod 750 /var/www/$domain
+chmod -R 770 /var/www/$domain/html
+chmod 750 /var/www/$domain/log
+
+group=$company
+adduser $user --force-badname --quiet <<EOF >>/dev/null 2>&1
+$passwd
+$passwd
+EOF
+addgroup $group --force-badname --quiet
+
+usermod -a -G "$group" "$user"
+chown root:$group /var/www/$domain
+chown -R $user:$group /var/www/$domain/html
+
+echo -e "
+Match Group $group
+        ForceCommand internal-sftp
+        PasswordAuthentication yes
+        ChrootDirectory /var/www/$domain
+        PermitTunnel no
+        PermitTTY no
+        AllowAgentForwarding no
+        AllowTcpForwarding no
+        X11Forwarding no" >> /etc/ssh/sshd_config
+
+systemctl restart nginx
+systemctl restart sshd
