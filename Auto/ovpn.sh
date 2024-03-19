c84371ea337c2a70c16e701ef67a8de041065ed3
Moska90 <57809899+Moska90@users.noreply.github.com>
2024-03-19 18:56:31 +0100

    Check for EasyRSA direcotry

--- Auto/ovpn.sh
+++ Auto/ovpn.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+LRED="\e[91m"
+LGREEN="\e[92m"
+LYELLOW="\e[93m"
+LBLUE="\e[94m"
+LMAGENTA="\e[95m"
+LCYAN="\e[96m"
+LGREY="\e[97m"
+BOLD="\e[1m"
+RESET="\e[0m"
 
+STDCOLOR="\e[96m"
+ERRCOLOR="\e[91m"
+
+# Directories for .ovpn files
 KEY_DIR=/etc/openvpn/client
 OUTPUT_DIR=/etc/openvpn/client
 BASE_CONFIG=/etc/openvpn/client/client.conf
 
-cat $BASE_CONFIG \
+# Function for checks
+function pwdeasyrsa() {
+	if [[ $(pwd) != */EasyRSA* ]]; then
+		echo -e "$LRED Run the script from the EasyRSA directory"
+		exit 1
+	fi
+}
+
+
+# Cert and key creation for client
+
+
+
+# .ovpn creation for client
+#cat $BASE_CONFIG \
 	<(echo -e '<ca>') \
 	$KEY_DIR/ca.crt
 	<(echo -e '</ca>\n<cert>') \
